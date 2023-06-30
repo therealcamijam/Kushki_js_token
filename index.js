32ca54eb39006681bbb9755b50fa6236a7fedb38
therealcamijam <126881236+therealcamijam@users.noreply.github.com>
2023-06-29 21:57:01 -0500

    index.js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 var kushki = new Kushki({
-    merchantId: '4a709fa929f14bd696c6fe138c936e37', 
-    inTestEnvironment: true,
-  });
+  merchantId: '4a709fa929f14bd696c6fe138c936e37', 
+  inTestEnvironment: true,
+});
 
 const Kushki_Form = document.getElementById("payment-form");
-  Kushki_Form.addEventListener("submit", (e) => {
-    e.preventDefault();
+Kushki_Form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const formvalues = ["name", "number", "expiryMonth","expiryYear", "cvc"].reduce((accum, cur) => ({
-      [cur]: document.querySelector(`input[name='${cur}']`).value,
-      ...accum,
-      }), {})
-    console.log(formvalues)
-     var callback = function(response) {
-    if(!response.code){
+  const formvalues = ["name", "number", "expiryMonth", "expiryYear", "cvc","valor"].reduce((accum, cur) => ({
+    [cur]: document.querySelector(`input[name='${cur}']`).value,
+    ...accum,
+  }), {});
+  
+  console.log(formvalues);
+  
+  var callback = function(response) {
+    if (!response.code) {
       console.log(response);
+      alert("Solicitud Exitosa!\nResponse: " + JSON.stringify(response));
     } else {
-     console.error('Error: ',response.error, 'Code: ', response.code, 'Message: ',response.message);
+      console.error('Error: ', response.error, 'Code: ', response.code, 'Message: ', response.message);
+      alert("Error en Solicitud!\nError: " + response.error + "\nCode: " + response.code + "\nMessage: " + response.message);
     }
-  }
+  };
   
   kushki.requestToken({
     amount: '4999',
     currency: "PEN",
     card: {
-     ...formvalues
-  },
+      ...formvalues
+    },
   }, callback);
-    // handle submit
-  });
+});
+
